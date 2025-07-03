93b6bc45b8040d8c37f0042aeacb35b2ead33a90
FLY123 <akfakf0509@naver.com>
2025-07-03 18:36:14 +0900

    release: some refactors (#16)
    * feat: rework feature1
    
    * refactor: feature
    
    * ci: merge with app (#17)

--- src/feature.js
+++ src/feature.js
@@ -1,5 +1,2 @@
-console.log("new feature code here");
-console.log("this is a new feature");
-console.log("hotfix");
-console.log("another hotfix");
-console.log("refactor hotfix code");
+console.log("rewritten feature code here");
+console.log("refactored feature code");
